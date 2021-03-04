3420b9079341a871ce8cbede9a056e6c5155f777
Christian <chrnielsen2003@gmail.com>
2021-03-04 14:23:15 +0100

    gr

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -43,7 +43,7 @@ export default function LogIn() {
 				<div className="touch">
 					<ion-icon name="finger-print-outline"></ion-icon>
 				</div>
-				<p className="oneTouch">One-Touch LogIn</p>
+				<p className="oneTouch">One-Touch LuyigduhvcfdxuydvfvfdbuhfgvduivfdguhkvfdguuyifvdgfvdbgfuhogIn</p>
 			</section>
 		</article>
 	);
